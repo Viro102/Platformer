2d02ed25d22b3cc254fb6a8d316b2b91254a4127
Adam Virostek <adam.virostek@gmail.com>
2021-11-20 16:01:15 +0100

    added walls

---
+++ src/Wall.java
@@ -0,0 +1,28 @@
+import java.awt.*;
+
+public class Wall {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+
+    private Rectangle hitBox;
+
+    public Wall(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+        hitBox = new Rectangle(x, y, width, height);
+
+    }
+
+    public void draw(Graphics2D gtd) {
+        gtd.setColor(Color.BLACK);
+        gtd.drawRect(x, y, width, height);
+        gtd.setColor(Color.WHITE);
+        gtd.fillRect(x + 1, y + 1, width - 2, height - 2);
+
+    }
+}
