4ebfcaee5aa838a3e76b8fda6842ea7c0a854546
Adam Virostek <adam.virostek@gmail.com>
2021-11-27 16:02:16 +0100

    add keybindings

--- src/KeyChecker.java
+++ src/KeyChecker.java
@@ -1,2 +1,71 @@
+import javax.swing.*;
+import java.awt.event.*;
+
 public class KeyChecker {
+
+    Action upAction;
+    Action downAction;
+    Action leftAction;
+    Action rightAction;
+
+    JLabel player;
+
+    public KeyChecker() {
+        upAction = new UpAction();
+        downAction = new DownAction();
+        leftAction = new LeftAction();
+        rightAction = new RightAction();
+
+        player.getInputMap().put(KeyStroke.getKeyStroke('w'), "upAction");
+        player.getActionMap().put("upAction", upAction);
+
+        player.getInputMap().put(KeyStroke.getKeyStroke('s'), "downAction");
+        player.getActionMap().put("downAction", downAction);
+
+        player.getInputMap().put(KeyStroke.getKeyStroke('a'), "leftAction");
+        player.getActionMap().put("leftAction", leftAction);
+
+        player.getInputMap().put(KeyStroke.getKeyStroke('d'), "rightAction");
+        player.getActionMap().put("rightAction", rightAction);
+    }
+
+    public class UpAction extends AbstractAction {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            player.jump();
+
+        }
+
+    }
+
+    public class DownAction extends AbstractAction {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            player.crouch();
+
+        }
+
+    }
+
+    public class LeftAction extends AbstractAction {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            player.goLeft();
+
+        }
+
+    }
+
+    public class RightAction extends AbstractAction {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            player.goRight();
+
+        }
+
+    }
 }
