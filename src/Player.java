8e10a020ed05504881be4af34115457bc43c64cf
Adam Virostek <adam.virostek@gmail.com>
2022-01-05 16:02:19 +0100

    add collisions

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
@@ -23,13 +24,14 @@ public class Player {
 
     public static Player getInstance() {
         if (Player.instance == null) {
-            Player.instance = new Player(48, 96, 48, 96);
+            Player.instance = new Player(32, 96, 32, 64);
         }
 
         return Player.instance;
     }
 
     public void draw(Graphics g) {
+        g.setColor(Color.GREEN);
         g.fillRect(this.x, this.y, this.width, this.height);
     }
 
@@ -38,7 +40,15 @@ public class Player {
     }
 
     public void moveUp() {
-        this.ySpeed--;
+        // odrazi sa len vtedy ak je hrac na plosine
+        hitbox.y++;
+        for (Wall wall : GamePanel.getInstance().getWalls()) {
+            if (hitbox.intersects(wall.getHitbox())) {
+                this.ySpeed = -8;
+            }
+        }
+        hitbox.y--;
+
     }
 
     public void moveLeft() {
@@ -47,25 +57,53 @@ public class Player {
 
     public void set() {
 
-        if (this.xSpeed > 0 && this.xSpeed < 0.75) {
+        // osetrenie "klzania hraca"
+        if ((this.xSpeed < 0 && this.xSpeed > -0.75) || (this.xSpeed > 0 && this.xSpeed < 0.75)) {
             this.xSpeed = 0;
         }
 
-        if (this.xSpeed < 0 && this.xSpeed > -0.75) { // osetrenie "klzania hraca"
-            this.xSpeed = 0;
-        }
-
-        if (this.xSpeed > 7) { // nastavenie max rychlosti
+        // nastavenie max rychlosti
+        if (this.xSpeed > 7) {
             this.xSpeed = 7;
         }
 
-        if (this.xSpeed < -7) { // nastavenie max rychlosti na opacnu stranu
+        // nastavenie max rychlosti na opacnu stranu
+        if (this.xSpeed < -7) {
             this.xSpeed = -7;
         }
 
         this.xSpeed *= 0.9;
 
-        // ySpeed += 0.4; // gravitacia
+        // gravitacia
+        this.ySpeed += 0.4;
+
+        // Horizontalna kolizia
+        hitbox.x += xSpeed;
+        for (Wall wall : GamePanel.getInstance().getWalls()) {
+            if (hitbox.intersects(wall.getHitbox())) {
+                hitbox.x -= xSpeed;
+                while (!wall.getHitbox().intersects(hitbox)) {
+                    hitbox.x += Math.signum(xSpeed);
+                }
+                hitbox.x -= Math.signum(xSpeed);
+                xSpeed = 0;
+                x = hitbox.x;
+            }
+        }
+
+        // Vertikalna kolizia
+        hitbox.y += ySpeed;
+        for (Wall wall : GamePanel.getInstance().getWalls()) {
+            if (hitbox.intersects(wall.getHitbox())) {
+                hitbox.y -= ySpeed;
+                while (!wall.getHitbox().intersects(hitbox)) {
+                    hitbox.y += Math.signum(ySpeed);
+                }
+                hitbox.y -= Math.signum(ySpeed);
+                ySpeed = 0;
+                y = hitbox.y;
+            }
+        }
 
         this.x += this.xSpeed;
         this.y += this.ySpeed;
@@ -74,5 +112,19 @@ public class Player {
         this.hitbox.y = this.y;
     }
 
-    // TODO Kolizie
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setXSpeed(double x) {
+        this.xSpeed = x;
+    }
+
+    public void setYSpeed(double y) {
+        this.ySpeed = y;
+    }
 }
--- src/Wall.java
+++ src/Wall.java
@@ -1,21 +1,26 @@
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 
 public class Wall {
 
     private int x;
     private int y;
 
+    private Rectangle hitBox;
+
     public Wall(int x, int y) {
         this.x = x;
         this.y = y;
-
+        this.hitBox = new Rectangle(this.x, this.y, 32, 32);
     }
 
-    public void makeWalls(Graphics g) {
-        for (int i = 0; i < 16; i++) {
-            g.drawRect(this.x + (48 * i), this.y, 48, 48);
-        }
+    public void draw(Graphics g) {
+        g.setColor(Color.BLACK);
+        g.fillRect(this.x, this.y, 32, 32);
     }
 
-    // TODO Kolizie, nahodne generovanie plosin
+    public Rectangle getHitbox() {
+        return this.hitBox;
+    }
 }
