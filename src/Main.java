957aa6b03e71b1b2cf98517ff78eeb1a8842add4
Adam Virostek <adam.virostek@gmail.com>
2022-01-05 16:02:47 +0100

    add kind of a main menu

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
-public class Main {
-    private static MainFrame mainFrame;
-    private static GamePanel panel;
+import javax.swing.JOptionPane;
 
+public class Main {
     public static void main(String[] args) throws Exception {
-        mainFrame = new MainFrame();
-        panel = GamePanel.getInstance();
-        mainFrame.add(panel);
-        mainFrame.setVisible(true);
-        mainFrame.pack();
-        panel.startGameThread();
+        String[] options = { "Start hry", "Zavri hru" };
+        int n = JOptionPane.showOptionDialog(null,
+                "Ovladanie A - dolava, D - doprava, W - skok\nTvojou ulohou je sa dostat ku modremu policku\nVyhybaj sa cervenym objektom",
+                "Uvodne info",
+                JOptionPane.OK_CANCEL_OPTION,
+                JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+
+        if (n == 0) {
+            MainFrame mainFrame = new MainFrame();
+            GamePanel panel = GamePanel.getInstance();
+            mainFrame.add(panel);
+            mainFrame.setVisible(true);
+            mainFrame.pack();
+            panel.startGameThread();
+        } else {
+            System.exit(0);
+        }
     }
 }
